3e724afabb14fc3ee30d10f0bf8f818470d225ef
matrix23 <e9jetxinallla@gmail.com>
2016-01-13 17:26:26 +0900

    同時表示

---
+++ KinectV2/main.cpp
@@ -0,0 +1,211 @@
+﻿#include <iostream>
+#include <sstream>
+
+#include <Kinect.h>
+#include <opencv2\opencv.hpp>
+
+#include <atlbase.h>
+using namespace std;
+// 次のように使います
+// ERROR_CHECK( ::GetDefaultKinectSensor( &kinect ) );
+// 書籍での解説のためにマクロにしています。実際には展開した形で使うことを検討してください。
+#define ERROR_CHECK( ret )  \
+    if ( (ret) != S_OK ) {    \
+        std::stringstream ss;	\
+        ss << "failed " #ret " " << std::hex << ret << std::endl;			\
+        throw std::runtime_error( ss.str().c_str() );			\
+    }
+
+class KinectApp
+{
+private:
+
+    // Kinect SDK
+    CComPtr<IKinectSensor> kinect = nullptr;
+
+    CComPtr<IInfraredFrameReader> infraredFrameReader = nullptr;
+
+    int infraredWidth;
+    int infraredHeight;
+
+    // 表示用
+    std::vector<UINT16> infraredBuffer;
+	CComPtr<IDepthFrameReader> depthFrameReader = nullptr;
+
+	// 表示部分
+	int depthWidth;
+	int depthHeight;
+
+	vector<UINT16> depthBuffer;
+	int depthPointX;
+	int depthPointY;
+
+	const char* DepthWindowName = "Depth Image";
+
+public:
+
+    ~KinectApp()
+    {
+        // Kinectの動作を終了する
+        if ( kinect != nullptr ){
+            kinect->Close();
+        }
+    }
+
+    // 初期化
+    void initialize()
+    {
+        // デフォルトのKinectを取得する
+        ERROR_CHECK( ::GetDefaultKinectSensor( &kinect ) );
+        ERROR_CHECK( kinect->Open() );
+
+        // 赤外線画像リーダーを取得する
+        CComPtr<IInfraredFrameSource> infraredFrameSource;
+        ERROR_CHECK( kinect->get_InfraredFrameSource( &infraredFrameSource ) );
+        ERROR_CHECK( infraredFrameSource->OpenReader( &infraredFrameReader ) );
+
+        // 赤外線画像のサイズを取得する
+        CComPtr<IFrameDescription> infraredFrameDescription;
+        ERROR_CHECK( infraredFrameSource->get_FrameDescription(
+                                                &infraredFrameDescription ) );
+        ERROR_CHECK( infraredFrameDescription->get_Width( &infraredWidth ) );
+        ERROR_CHECK( infraredFrameDescription->get_Height( &infraredHeight ) );
+
+        // バッファーを作成する
+        infraredBuffer.resize( infraredWidth * infraredHeight );
+
+
+		// Depthリーダーを取得する
+		CComPtr<IDepthFrameSource> depthFrameSource;
+		ERROR_CHECK(kinect->get_DepthFrameSource(&depthFrameSource));
+		ERROR_CHECK(depthFrameSource->OpenReader(&depthFrameReader));
+
+		// Depth画像のサイズを取得する
+		CComPtr<IFrameDescription> depthFrameDescription;
+		ERROR_CHECK(depthFrameSource->get_FrameDescription(
+			&depthFrameDescription));
+		ERROR_CHECK(depthFrameDescription->get_Width(&depthWidth));
+		ERROR_CHECK(depthFrameDescription->get_Height(&depthHeight));
+
+		depthPointX = depthWidth / 2;
+		depthPointY = depthHeight / 2;
+
+		// Depthの最大値、最小値を取得する
+		UINT16 minDepthReliableDistance;
+		UINT16 maxDepthReliableDistance;
+		ERROR_CHECK(depthFrameSource->get_DepthMinReliableDistance(
+			&minDepthReliableDistance));
+		ERROR_CHECK(depthFrameSource->get_DepthMaxReliableDistance(
+			&maxDepthReliableDistance));
+		std::cout << "Depth最小値       : " << minDepthReliableDistance << endl;
+		cout << "Depth最大値       : " << maxDepthReliableDistance << endl;
+
+		// バッファーを作成する
+		depthBuffer.resize(depthWidth * depthHeight);
+    }
+
+    void run()
+    {
+        while ( 1 ) {
+            update();
+            draw();
+
+            auto key = cv::waitKey( 10 );
+            if ( key == 'q' ){
+                break;
+            }
+        }
+    }
+
+private:
+
+    // データの更新処理
+    void update()
+    {
+        updateInfrared();
+		updateDepthFrame();
+    }
+
+    void updateInfrared()
+    {
+        // フレームを取得する
+        CComPtr<IInfraredFrame> infraredFrame;
+        auto ret = infraredFrameReader->AcquireLatestFrame( &infraredFrame );
+        if ( FAILED( ret ) ){
+            return;
+        }
+
+        // データを取得する
+        ERROR_CHECK( infraredFrame->CopyFrameDataToArray(
+            infraredBuffer.size(), &infraredBuffer[0] ) );
+    }
+
+	void updateDepthFrame()
+	{
+		// Depthフレームを取得
+		CComPtr<IDepthFrame> depthFrame;
+		auto ret = depthFrameReader->AcquireLatestFrame(&depthFrame);
+		if (ret != S_OK){
+			return;
+		}
+
+		// データを取得する
+		ERROR_CHECK(depthFrame->CopyFrameDataToArray(
+			depthBuffer.size(), &depthBuffer[0]));
+
+
+	}
+    void draw()
+    {
+		drawInfraredFrame();
+		drawDepthFrame();
+    }
+
+	void drawInfraredFrame()
+	{
+		// カラーデータを表示する
+		cv::Mat colorImage(infraredHeight, infraredWidth,
+			CV_16UC1, &infraredBuffer[0]);
+		cv::imshow("Infrared Image", colorImage);
+	}
+
+	void drawDepthFrame()
+	{
+		// Depthデータを表示するかコレ?
+		cv::Mat depthImage(depthHeight, depthWidth, CV_8UC1);
+		// フィルタ後
+		cv::Mat depthAfter(depthHeight, depthWidth, CV_8UC1);
+		// Depthデータを0-255のグレーデータにする
+
+
+		for (int i = 0; i < depthImage.total(); ++i){
+			depthImage.data[i] = depthBuffer[i] * 255 / 8000;
+		}
+
+
+		// Depthデータのインデックスを取得して、その場所の距離を表示する
+		int index = (depthPointY * depthWidth) + depthPointX;
+		std::stringstream ss;
+		ss << depthBuffer[index] << "mm X=" << depthPointX << " Y= " << depthPointY;
+
+		cv::circle(depthImage, cv::Point(depthPointX, depthPointY), 3,
+			cv::Scalar(255, 255, 255), 2);
+		cv::putText(depthImage, ss.str(), cv::Point(depthPointX, depthPointY),
+			0, 0.5, cv::Scalar(255, 255, 255));
+
+		cv::imshow(DepthWindowName, depthImage);
+
+	}
+};
+
+void main()
+{
+    try {
+        KinectApp app;
+        app.initialize();
+        app.run();
+    }
+    catch ( std::exception& ex ){
+        std::cout << ex.what() << std::endl;
+    }
+}
